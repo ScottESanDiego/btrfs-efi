fe24f5a176b11fdc35616412b469fca1d20745ee
Mark Harmstone <mark@harmstone.com>
2025-11-11 16:48:40 +0000

    clang-arm.cmake: fix

--- clang-arm.cmake
+++ clang-arm.cmake
@@ -3,8 +3,8 @@ SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_C_COMPILER clang)
 SET(CMAKE_SYSTEM_PROCESSOR ARM)
 
-SET(CLANG_TARGET_TRIPLE armv7l-w64-mingw32)
-SET(CMAKE_C_COMPILER_TARGET armv7l-w64-mingw32)
+SET(CLANG_TARGET_TRIPLE thumbv7-w64-mingw32)
+SET(CMAKE_C_COMPILER_TARGET thumbv7-w64-mingw32)
 
 set(CMAKE_EXE_LINKER_FLAGS "-static -fuse-ld=lld")
 
@@ -13,4 +13,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # choose a CPU that supports integer divisions
-set(CMAKE_C_FLAGS -mcpu=cortex-a72)
+set(CMAKE_C_FLAGS "-mcpu=cortex-a15 -mfpu=none")
+
+# FIXME - this doesn't work, as Clang mistakenly outputs a file with machine type 1c4 rather than 1c2
